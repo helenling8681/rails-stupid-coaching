09e8d7552cbfc2dfa7fd78d69aee61478d25bbf2
helen ling <hling.mim2014@london.edu>
2020-07-27 13:07:08 +0800

    qs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,19 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @question = params[:question]
+    @answer = display(@question)
+  end
+
+  def display(answer)
+    if answer == "I am going to work"
+      return "Great!"
+    elsif answer.chars.last == "?"
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
 end
